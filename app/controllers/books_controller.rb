04b1900bd426e60207eb90deee6ca00ab846b509
TakanoriShima <quark2galaxy@gmail.com>
2022-02-21 18:53:30 +0900

    Rspec & modified done

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,6 +26,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
+      flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book)
     else
       render :edit
