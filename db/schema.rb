6e34a994807e1a205accfd617aedf2f36cbeee4d
EC2 Default User <ec2-user@ip-172-31-33-140.ap-northeast-1.compute.internal>
2022-12-08 00:15:32 +0000

    local mysql

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2022_02_21_081646) do
 
-  create_table "books", force: :cascade do |t|
+  create_table "books", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
